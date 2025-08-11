c2c028235da02255b1e189c8782e62138957a80c
auranovad <auranovad@gmail.com>
2025-08-10 23:34:23 -0500

    chore(vite): ignore local env & tsbuildinfo/bak; accept NEXT_PUBLIC_ env prefix

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // acepta variables VITE_ y NEXT_PUBLIC_ en Vite
   envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
 })
