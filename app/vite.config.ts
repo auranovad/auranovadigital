dd15027c6d5d2fbb16e77d753f749d3648ca1286
auranovad <auranovad@gmail.com>
2025-08-10 22:35:21 -0500

    chore(vite): accept NEXT_PUBLIC_ and VITE_ env prefixes

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,7 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
-});
+  envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
+})
