8f22029f64b5e16aca1f069980b49fa1ccbfced8
auranovad <auranovad@gmail.com>
2025-08-10 23:51:07 -0500

    chore(vite): accept NEXT_PUBLIC_ and VITE_ env prefixes; ignore local files (#2)
    * chore(vite): accept NEXT_PUBLIC_ and VITE_ env prefixes
    
    * chore(vite): ignore local env & tsbuildinfo/bak; accept NEXT_PUBLIC_ env prefix

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,8 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
-});
+  // acepta variables VITE_ y NEXT_PUBLIC_ en Vite
+  envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
+})
