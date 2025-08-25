5fc6e8ae13bc10ef180e16a8bd6d4ee2ced42de2
auranovad <auranovad@gmail.com>
2025-08-24 22:01:31 -0500

    build(vite): base='' to fix prod blank screen

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,11 +4,12 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }, // opcional
+  base: '', // 🔸 importantísimo para Vercel / assets
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
     },
   },
+  server: { port: 3000 }, // opcional
 })
